e2dd9964a4388b552f250edc13667200bed935e5
jayane_santos <100379675+projetosjay@users.noreply.github.com>
2023-03-15 16:38:52 -0300

    Add files via upload

---
+++ desafio/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace desafio;
+class Program
+{
+    static void Main(string[] args)
+    
+     {
+        Aluno aluno1 = new Aluno();
+
+        Console.WriteLine("Digite seu nome: ");
+        string? nome = Console.ReadLine();
+        Console.WriteLine("Seu nome é: " + nome);
+        Console.Write("Digite um numero: ");
+        aluno1.nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("O numero digitado foi: ");
+        Console.Write("Digite um numero: ");
+        aluno1.nota2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("O numero digitado foi: ");
+
+        
+        aluno1.mensagem();
+    }
+}
