63db209bb7f89ddd72bf582a6632bf34ce5c85e7
jayane_santos <100379675+projetosjay@users.noreply.github.com>
2023-03-15 16:31:29 -0300

    Add files via upload

--- exercicioaluno/Program.cs
+++ exercicioaluno/Program.cs
@@ -1,22 +1,16 @@
-﻿namespace ExercicioAluno;
+﻿namespace exercicioaluno;
 class Program
 {
     static void Main(string[] args)
     
      {
         Aluno aluno1 = new Aluno();
+        aluno1.nome = "jayane santos";
+        aluno1.nota1 = 5;
+        aluno1.nota2 = 9;
 
-        Console.WriteLine("Digite seu nome: ");
-        string? nome = Console.ReadLine();
-        Console.WriteLine("Seu nome é: " + nome);
-        Console.Write("Digite um numero: ");
-        aluno1.nota1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("O numero digitado foi: ");
-        Console.Write("Digite um numero: ");
-        aluno1.nota2 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("O numero digitado foi: ");
+        aluno1.mensagem();        
 
-        
-        aluno1.mensagem();
+
     }
 }
