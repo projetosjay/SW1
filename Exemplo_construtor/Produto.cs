f04f05d4638cd24ade081842703e3654df2bf57f
jayane_santos <100379675+projetosjay@users.noreply.github.com>
2023-03-29 16:58:32 -0300

    Add files via upload
    Construtor

---
+++ Exemplo_construtor/Produto.cs
@@ -0,0 +1,42 @@
+﻿namespace Exemplo_construtor;
+class Produto
+{
+   public Produto(){
+    this.Nome = "XTPO";
+    this.Preco = 1;
+   }
+
+      public Produto(double preco){
+    this.Nome = "NÃO INFORMADO";
+    this.Preco = preco;
+   }
+
+      public Produto(String nome, double desconto){
+    this.Nome = nome;
+    this.Preco = 10- 10*desconto/100;
+   }
+
+public string? Nome{get;set;}
+
+public double Preco{get;set;}
+
+public void mostraDados(){
+    Console.WriteLine("Nome: " + this.Nome);
+    Console.WriteLine("Preço: " + this.Preco);
+
+}
+
+public void AlteraPreco(double preco){
+    if(preco >= 0){
+            this.Preco = preco;
+    }
+    else{
+        Console.WriteLine ("VALOR INVÁLIDO");
+        }
+
+}
+public void AlteraNome(string nome){
+    this.Nome = nome;
+
+}
+}
---
+++ Exemplo_construtor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exemplo_construtor;
+class Program
+{
+    static void Main(string[] args)
+    {
+    //Produto p1 = new Produto();
+    Produto p1 = new Produto ("abc", 10);
+    p1.mostraDados();
+    }
+}
