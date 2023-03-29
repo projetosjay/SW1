d7d2b5c18b389fa23e9e89fd350cd200b720b85c
jayane_santos <100379675+projetosjay@users.noreply.github.com>
2023-03-29 16:27:00 -0300

    Add files via upload
    Método assessor.

---
+++ EXEMPLO_ACESSOR/Produto.cs
@@ -0,0 +1,27 @@
+﻿namespace EXEMPLO_ACESSOR;
+class Produto
+{
+public string? Nome{get;set;}
+
+public double Preco{get;set;}
+
+public void mostraDados(){
+    Console.WriteLine("Nome: " + this.Nome);
+    Console.WriteLine("Preço: " + this.Preco);
+
+}
+
+public void AlteraPreco(double preco){
+    if(preco >= 0){
+            this.Preco = preco;
+    }
+    else{
+        Console.WriteLine ("VALOR INVÁLIDO");
+        }
+
+}
+public void AlteraNome(string nome){
+    this.Nome = nome;
+
+}
+}
---
+++ EXEMPLO_ACESSOR/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EXEMPLO_ACESSOR;
+class Program
+{
+    static void Main(string[] args)
+    {
+    Produto p1 = new Produto();
+    p1.AlteraPreco(50);
+    p1.AlteraNome("PARAFUSO");
+
+    p1.mostraDados();
+    }
+}
