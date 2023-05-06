e70838024625e50996b545299e0dd6d8e9885d61
jayane_santos <100379675+projetosjay@users.noreply.github.com>
2023-05-05 23:38:41 -0300

    Atividade bimestral

---
+++ Emprestimo/Emprestimo.cs
@@ -0,0 +1,29 @@
+namespace Emprestimo;
+class Emprestimo
+{
+    public double saldo {get;set;}
+
+    public double dinheiro {get;set;}
+
+    public double meses {get;set;}
+
+
+        public void depositar(double valor){
+        this.saldo += valor;
+    }
+
+
+    public void saque (double valor){
+    this.saldo -= valor;
+}
+
+
+public double emprestimo(){
+    return this.saldo = this.dinheiro / this.meses;
+}
+
+public double juros(){
+     return  this.saldo = this.dinheiro * 0.06;
+}
+
+}
---
+++ Emprestimo/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Emprestimo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Atividade Avaliativa");
+        Emprestimo agencia = new Emprestimo();
+
+        agencia.meses = 12;
+
+        agencia.dinheiro = 1000;
+
+        agencia.depositar(0);
+
+        agencia.saque(0);
+        
+
+        double saldo = (agencia.juros() + agencia.emprestimo())* agencia.meses;
+        Console.WriteLine("Total é R$" + saldo);
+
+        
+    }
+}    
