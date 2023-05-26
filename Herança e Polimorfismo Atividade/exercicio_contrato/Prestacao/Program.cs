d45054652fbc596a28eec529a01bac86ada095c5
jayane_santos <100379675+projetosjay@users.noreply.github.com>
2023-05-26 16:31:22 -0300

    Add files via upload

---
+++ Herança e Polimorfismo Atividade/exercicio_contrato/Prestacao/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Prestacao;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Fisica fisica = new Fisica();
+        Juridica juridica = new Juridica();
+
+       /* fisica.Detalhes(){
+            fisica.idade = 12;
+            fisica.cpf = 123456789;
+            juridica.cnpj = 40;
+            juridica.inscricao = 12;
+            fisica.calcularP = 40:
+
+        };*/
+        
+
+
+        Console.WriteLine("Valor da Prestação: " + juridica.calcularP() );
+        Console.WriteLine();
+
+    }
+}
